8de6b51a4860b06d04e3a8c8d8d7713ba409d905
Grendel Yang <stevenygd@gmail.com>
2016-07-06 12:50:31 -0400

    Add the coexistence analysis notebook for coco data. Fix the preprocess.sh from absolute path.

--- preprocess.sh
+++ preprocess.sh
@@ -2,7 +2,7 @@
 for i in *.jpg
 
 do
-	/usr/bin/gm mogrify -resize 256x256^ "$i"
+	gm mogrify -resize 256x256^ "$i"
 	convert "$i" -resize 256x256^ -gravity Center -crop 224x224+0+0 "$i"
 	echo "$i"
 done
